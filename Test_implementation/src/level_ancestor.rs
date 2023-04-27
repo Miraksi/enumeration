0e3a78d7f6ffa3983e64cfc115312b67a9e847f1
Arne Prinzler <arne.prinzler@stud.uni-goettingen.de>
2023-04-27 19:41:01 +0200

    added construction for ladders

--- Test_implementation/src/level_ancestor.rs
+++ Test_implementation/src/level_ancestor.rs
@@ -32,11 +32,14 @@ impl Ladders {
         self.dfs_depth(self.root, 0);
         self.leaf_depth.rdxsort();
         self.leaf_depth.reverse();
+        let original_parent = self.parent.clone();
 
         for i in 0..self.leaf_depth.len() {
             let (_h, p) = self.leaf_depth[i];
             self.cut(p);
         }
+        self.parent = original_parent;
+        self.path_to_ladders();
     }
 
     fn dfs_depth(&mut self, p: usize, current_depth: usize) {
@@ -51,18 +54,28 @@ impl Ladders {
     }
 
     fn cut(&mut self, mut p: usize) {
-        let mut ladder: Vec<usize> = vec![p];
+        let mut long_path: Vec<usize> = vec![p];
         while self.parent[p] != p {
             p = self.parent[p];
-            ladder.push(p);
+            long_path.push(p);
             for child in self.children[p].iter() {
                 if *child != p {
                     self.parent[*child] = *child;
                 }    
             }
         }
-        ladder.reverse();
-        self.ladders.push(ladder);
+        self.ladders.push(long_path);  // this will add a reverse long_path 
+    }
+
+    fn path_to_ladders(&mut self) {
+        for  i in 0..self.ladders.len() {
+            let h = self.ladders[i].len();
+            let mut top_node = self.ladders[i][h-1];
+            for _i in 0..h {
+                top_node = self.parent[top_node];
+                self.ladders[i].push(top_node);
+            }
+        }
     }
 }
 
