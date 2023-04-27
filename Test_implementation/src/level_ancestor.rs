6200a1e88cd6e584223f35f4c7a5545160ac28ea
Arne Prinzler <arne.prinzler@stud.uni-goettingen.de>
2023-04-27 18:59:25 +0200

    finished long-path decomposition

--- Test_implementation/src/level_ancestor.rs
+++ Test_implementation/src/level_ancestor.rs
@@ -11,8 +11,8 @@ pub struct Ladders {
     root: usize,
     parent: Vec<usize>,
     children: Vec<Vec<usize>>,
-    ladders: Vec<Vec<usize>>,
-    pub leaf_height: Vec<(usize, usize)>, // (height of leaf, index of leaf)
+    pub ladders: Vec<Vec<usize>>,        // pub for testing
+    pub leaf_depth: Vec<(usize, usize)>, // (depth of leaf, index of leaf) // pub for testing
 }
 
 impl Ladders {
@@ -22,21 +22,47 @@ impl Ladders {
             parent: parent,
             children: children,
             ladders: Vec::new(),
-            leaf_height: Vec::new(),
+            leaf_depth: Vec::new(),
         };
-        new.dfs_height(root, 0);
-        new.leaf_height.rdxsort();
+        new.compute_ladders();
         return new;
     }
-    fn dfs_height(&mut self, p: usize, current_height: usize) {
+
+    fn compute_ladders(&mut self) {
+        self.dfs_depth(self.root, 0);
+        self.leaf_depth.rdxsort();
+        self.leaf_depth.reverse();
+
+        for i in 0..self.leaf_depth.len() {
+            let (_h, p) = self.leaf_depth[i];
+            self.cut(p);
+        }
+    }
+
+    fn dfs_depth(&mut self, p: usize, current_depth: usize) {
         if self.children[p].len() == 0 {
-            self.leaf_height.push((current_height, p));
+            self.leaf_depth.push((current_depth, p));
             return;
         }
         for i in 0..self.children[p].len() {
             let child = self.children[p][i];
-            self.dfs_height(child, current_height + 1);
+            self.dfs_depth(child, current_depth + 1);
         }
+    }
+
+    fn cut(&mut self, mut p: usize) {
+        let mut ladder: Vec<usize> = vec![p];
+        while self.parent[p] != p {
+            p = self.parent[p];
+            ladder.push(p);
+            for child in self.children[p].iter() {
+                if *child != p {
+                    self.parent[*child] = *child;
+                }    
+            }
+        }
+        ladder.reverse();
+        self.ladders.push(ladder);
     }
 }
 
--- Test_implementation/src/main.rs
+++ Test_implementation/src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     let parent: Vec<usize> = vec![0,0,1,0,3,3,2];
     let children: Vec<Vec<usize>> = vec![vec![1,3], vec![2], vec![6], vec![4,5], Vec::new(), Vec::new(), Vec::new()];
     let lad = Ladders::new(parent, children, 0);
-    println!("{:?}", lad.leaf_height);
+    println!("leaf_depth {:?}", lad.leaf_depth);
+    println!("ladders {:?}", lad.ladders);
 }
