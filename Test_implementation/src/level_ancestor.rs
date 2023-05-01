714a66f917f4ba4a11b35d759948849913b0f512
Arne Prinzler <arne.prinzler@stud.uni-goettingen.de>
2023-05-01 14:05:55 +0200

    refactored level_ancestor and added Node struct

--- Test_implementation/src/level_ancestor.rs
+++ Test_implementation/src/level_ancestor.rs
@@ -1,93 +1,125 @@
 extern crate rdxsort;
 use rdxsort::*;
+use std::collections::HashMap;
+use std::slice::Iter;
 
 fn log_floor(x: u32) -> u32 {   // TODO outsource this code into a module
     return u32::BITS - x.leading_zeros() - 1;
 }
+
+struct Node {
+    parent: usize,
+    children: Vec<usize>,
+    ladder: usize,
+    ladder_idx: usize,
+}
+impl Node {
+    fn new(parent: usize, children: Vec<usize>) -> Self {
+        Self{
+            parent: parent,
+            children: children,
+            ladder: 0,
+            ladder_idx: 0,
+        }
+    }
+}
+
 // we maybe need lifetimes here
 pub struct Ladders {
+    n: usize,
     root: usize,
-    parent: Vec<usize>,
-    children: Vec<Vec<usize>>,
-    pub ladders: Vec<Vec<usize>>,        // pub for testing
+    nodes: Vec<Node>,
+    pub ladders: Vec<Vec<usize>>,        // ladders are in reverse order // pub for testing
     pub leaf_depth: Vec<(usize, usize)>, // (depth of leaf, index of leaf) // pub for testing
-    ladder_of_node: Vec<usize>,     // stores the index of the ladder on which a node lies
     jump_nodes: Vec<usize>,         // stores all jump_nodes (leaves of the macrotree)
+    jump_points: HashMap<usize, Vec<usize>>,    // stores all the jumppoints for a jump node
+                                                // we are allowed to stor this in a HashMap, 
+                                                // since inserting will still work in O(n) 
+                                                // (n beeing the number of nodes)
 }
 
 impl Ladders {
     pub fn new(parent: Vec<usize>, children: Vec<Vec<usize>>, root: usize) -> Self {
         let n = parent.len();
+        let nodes = compute_node_list(&parent, children);
         let mut new = Self {
+            n: n,
             root: root,
-            parent: parent,
-            children: children,
+            nodes: nodes,
             ladders: Vec::new(),
             leaf_depth: Vec::new(),
-            ladder_of_node: vec![0;n],
             jump_nodes: Vec::new(),
+            jump_points: HashMap::new(),
         };
-        new.compute_ladders();
-        new.find_jump_nodes(root, (log_floor(n as u32)/4) as usize);
+        new.compute_ladders(parent);
+        new.compute_jump_points();
         return new;
     }
 
-    fn compute_ladders(&mut self) {
+    fn compute_ladders(&mut self, mut parent: Vec<usize>) {
         self.dfs_depth(self.root, 0);
         self.leaf_depth.rdxsort();
         self.leaf_depth.reverse();
-        let original_parent = self.parent.clone();
 
         for i in 0..self.leaf_depth.len() {
             let (_h, p) = self.leaf_depth[i];
-            self.cut(p);
+            self.cut(p, &mut parent);
         }
-        self.parent = original_parent;
         self.path_to_ladders();
     }
 
     fn dfs_depth(&mut self, p: usize, current_depth: usize) {
-        if self.children[p].len() == 0 {
+        if self.nodes[p].children.len() == 0 {
             self.leaf_depth.push((current_depth, p));
             return;
         }
-        for i in 0..self.children[p].len() {
-            let child = self.children[p][i];
+        for i in 0..self.nodes[p].children.len() {
+            let child = self.ith_child(p,i);
             self.dfs_depth(child, current_depth + 1);
         }
     }
 
-    fn cut(&mut self, mut p: usize) {
+    fn cut(&mut self, mut p: usize, parent: &mut Vec<usize>) {
         let mut long_path: Vec<usize> = vec![p];
-        while self.parent[p] != p {
-            p = self.parent[p];
+        while parent[p] != p {
+            p = parent[p];
             long_path.push(p);
-            self.ladder_of_node[p] = self.ladders.len();
-            for child in self.children[p].iter() {
+            self.nodes[p].ladder = self.ladders.len();
+            for child in self.children_of(p) {
                 if *child != p {
-                    self.parent[*child] = *child;
+                    parent[*child] = *child;
                 }    
             }
         }
         self.ladders.push(long_path);  // this will add a reverse long_path 
     }
 
-    fn path_to_ladders(&mut self) {
+    fn path_to_ladders(&mut self) {    // ladders maybe have to be reversed, but change idx then
         for  i in 0..self.ladders.len() {
             let h = self.ladders[i].len();
             let mut top_node = self.ladders[i][h-1];
-            for _i in 0..h {
-                top_node = self.parent[top_node];
+            for j in 0..h {
+                top_node = self.parent_of(top_node);
                 self.ladders[i].push(top_node);
+                self.nodes[self.ladders[i][j]].ladder_idx = j; // set ladder index for noded on the ladder
             }
         }
     }
 
+    fn compute_jump_points(&mut self) {
+        let bound = (log_floor(self.n as u32)/4) as usize;
+        self.find_jump_nodes(self.root, bound); 
+        
+        for i in 0..self.jump_nodes.len() {
+            self.compute_jumps(self.jump_nodes[i]);
+        }
+    }
+
     fn find_jump_nodes(&mut self, root: usize, boundary: usize) -> usize{
-        let mut decendants = self.children[root].len();
+        let mut decendants = self.nodes[root].children.len();
         let mut child_decendants: usize = 0;
-        for i in 0..self.children[root].len() {
-            let child = self.children[root][i];
+        for i in 0..self.nodes[root].children.len() {
+            let child = self.ith_child(root, i);
             let tmp = self.find_jump_nodes(child, boundary);
             decendants += tmp;
             child_decendants = max(child_decendants, tmp);
@@ -97,6 +129,36 @@ impl Ladders {
         }
         return decendants;
     }
+
+    fn compute_jumps(&mut self, base: usize) {  // Maybe this should return a Vector
+        let mut jumps: Vec<usize> = vec![self.parent_of(base)];
+        let mut current: usize = self.parent_of(base);
+        let mut jump_size: usize = 2;
+        while jump_size < self.n {
+            
+        }
+    }
+
+    fn ith_child(&self, node: usize, child_idx: usize) -> usize { // maybe add Result-Type
+        return self.nodes[node].children[child_idx];
+    }
+
+    fn parent_of(&self, node: usize) -> usize {
+        return self.nodes[node].parent;
+    }
+
+    fn children_of(&self, node: usize) -> Iter<usize> {
+        return self.nodes[node].children.iter();
+    }
+}
+
+fn compute_node_list(parent: &Vec<usize>, children: Vec<Vec<usize>>) -> Vec<Node> {   
+    let mut list: Vec<Node> = Vec::new();
+    for p in parent.iter() {
+        let node = Node::new(*p, children[*p].clone());
+        list.push(node);
+    }
+    return list;
 }
 
 fn max(a: usize, b: usize) -> usize { // TODO outsource
