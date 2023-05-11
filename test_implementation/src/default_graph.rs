bb63f176152cbd66f4ee4d43bf2ad07012568443
Arne Prinzler <arne.prinzler@stud.uni-goettingen.de>
2023-05-11 14:47:36 +0200

    added cycle default cycles

--- test_implementation/src/default_graph.rs
+++ test_implementation/src/default_graph.rs
@@ -29,13 +29,15 @@ impl DefaultComp {
 pub struct DefaultGraph {
     pub components: Vec<DefaultComp>,
     pub default_edges: Vec<Vec<usize>>,
-    pub mapping: Vec<Option<usize>>,
+    pub comp_idx: Vec<Option<usize>>,   //maps the node to the the component on wich it lies
+    pub mapping: Vec<Option<usize>>,    //maps the node of the graph to the index in the component
 }
 impl DefaultGraph {
     pub fn new(delta: &Vec<HashMap<char, usize>>) -> Self {
         let mut new = Self{
             components: Vec::new(),
             default_edges: compute_default_graph(delta),
+            comp_idx: vec![None; delta.len()],
             mapping: vec![None; delta.len()],
         };
         new.compute_default_components();
@@ -50,13 +52,23 @@ impl DefaultGraph {
             let indep = self.calc_independent(&rev_default_edges, *root);
             self.components.push(indep);
         }
+        let mut visited: Vec<bool> = vec![false; self.default_edges.len()];
+        for p in 0..self.comp_idx.len() {
+            match self.mapping[p] {
+                Some(_) => continue,
+                None => self.find_cycle(p, &mut visited),
+            };
+        }
     }
 
     fn calc_independent(&mut self, rev_edges: &Vec<Vec<usize>>, root: usize) -> DefaultComp {
         let mut return_list: Vec<Vec<usize>> = vec![Vec::new()];
         let mut comp_mapping: Vec<usize> = Vec::new();
-        self.mapping[root] = Some(0);
+
+        self.mapping[root] = Some(0);       //TODO group theses operation inside a method
         comp_mapping.push(root);
+        self.comp_idx[root] = Some(self.components.len());
+
         let mut queue: Vec<usize> = vec![root];
 
         while !queue.is_empty() {
@@ -68,9 +80,10 @@ impl DefaultGraph {
                     edges.push(x);
                 }
                 else {
-                    self.mapping[*q] = Some(comp_mapping.len());     
+                    self.mapping[*q] = Some(comp_mapping.len());     //same here
                     edges.push(comp_mapping.len());
                     comp_mapping.push(*q);
+                    self.comp_idx[*q] = Some(self.components.len());
                     return_list.push(Vec::new());
                 }
             }
@@ -80,6 +93,36 @@ impl DefaultGraph {
         }
 
         return DefaultComp::new(CompType::Independent, return_list, comp_mapping);
+    }
+
+    fn find_cycle(&mut self, mut current: usize, visited: &mut Vec<bool>) {
+        let mut next = self.default_edges[current][0];
+        while !visited[next] {
+            visited[current] = true;
+            current = next;
+            next = self.default_edges[current][0];
+        }
+        let cycle = self.construct_cycle(next);
+    }
+
+    fn construct_cycle(&mut self, start: usize) -> DefaultComp {
+        let mut current = start;
+        let mut next = self.default_edges[current][0];
+        let mut idx: usize = 0;
+        let mut mapping: Vec<usize> = vec![current];
+        self.mapping[current] = idx;
+        self.comp_idx[current] = Some(self.components.len());
+        
+        while next != start {
+            current = next;
+            idx += 1;
+            mapping.push(current);
+            self.mapping[current] = idx;
+            self.comp_idx[current] = Some(self.components.len());
+
+            next = self.default_edges[current][0];
+        }
+        return DefaultComp::new(CompType::Cycle, Vec::new(), mapping);
     }
 }
 
