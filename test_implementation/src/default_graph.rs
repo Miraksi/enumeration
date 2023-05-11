1900ba9acceeb24924da03d71a6b81a49fb718cb
Arne Prinzler <arne.prinzler@stud.uni-goettingen.de>
2023-05-11 15:23:50 +0200

    added connected trees

--- test_implementation/src/default_graph.rs
+++ test_implementation/src/default_graph.rs
@@ -29,6 +29,7 @@ impl DefaultComp {
 pub struct DefaultGraph {
     pub components: Vec<DefaultComp>,
     pub default_edges: Vec<Vec<usize>>,
+    pub rev_default_edges: Vec<Vec<usize>>,
     pub comp_idx: Vec<Option<usize>>,   //maps the node to the the component on wich it lies
     pub mapping: Vec<Option<usize>>,    //maps the node of the graph to the index in the component
 }
@@ -37,19 +38,20 @@ impl DefaultGraph {
         let mut new = Self{
             components: Vec::new(),
             default_edges: compute_default_graph(delta),
+            rev_default_edges: Vec::new(),
             comp_idx: vec![None; delta.len()],
             mapping: vec![None; delta.len()],
         };
+        new.rev_default_edges = reverse_edges(&new.default_edges);
         new.compute_default_components();
         return new;
     }
 
     fn compute_default_components(&mut self) { //TODO think of a representation of default components
         let roots: Vec<usize> = find_roots(&self.default_edges);
-        let rev_default_edges = reverse_edges(&self.default_edges);
     
         for root in roots.iter() {
-            let indep = self.calc_independent(&rev_default_edges, *root);
+            let indep = self.calc_independent(*root);
             self.components.push(indep);
         }
         let mut visited: Vec<bool> = vec![false; self.default_edges.len()];
@@ -61,7 +63,7 @@ impl DefaultGraph {
         }
     }
 
-    fn calc_independent(&mut self, rev_edges: &Vec<Vec<usize>>, root: usize) -> DefaultComp {
+    fn calc_independent(&mut self, root: usize) -> DefaultComp {
         let mut return_list: Vec<Vec<usize>> = vec![Vec::new()];
         let mut comp_mapping: Vec<usize> = Vec::new();
 
@@ -74,7 +76,7 @@ impl DefaultGraph {
         while !queue.is_empty() {
             let p = queue.pop().unwrap();
             let mut edges: Vec<usize> = Vec::new();
-            for q in rev_edges[p].iter() {
+            for q in self.rev_default_edges[p].iter() {
                 queue.push(*q);
                 if let Some(x) = self.mapping[*q] {
                     edges.push(x);
@@ -102,27 +104,71 @@ impl DefaultGraph {
             current = next;
             next = self.default_edges[current][0];
         }
-        let cycle = self.construct_cycle(next);
+        let cycle = self.calc_cycle(next);
+        for p in cycle.mapping.iter() {
+            self.calc_connected(*p);
+        }
     }
 
-    fn construct_cycle(&mut self, start: usize) -> DefaultComp {
+    fn calc_cycle(&mut self, start: usize) -> DefaultComp {
         let mut current = start;
         let mut next = self.default_edges[current][0];
         let mut idx: usize = 0;
         let mut mapping: Vec<usize> = vec![current];
-        self.mapping[current] = idx;
+        self.mapping[current] = Some(idx);
         self.comp_idx[current] = Some(self.components.len());
         
         while next != start {
             current = next;
             idx += 1;
             mapping.push(current);
-            self.mapping[current] = idx;
+            self.mapping[current] = Some(idx);
             self.comp_idx[current] = Some(self.components.len());
 
             next = self.default_edges[current][0];
         }
         return DefaultComp::new(CompType::Cycle, Vec::new(), mapping);
+    }
+
+    fn calc_connected(&mut self, root: usize) {     //TODO clean up code
+        if self.rev_default_edges[root].len() == 1 {
+            return;
+        }
+        let mut edge_list: Vec<Vec<usize>> = vec![Vec::new()];
+        let mut comp_mapping: Vec<usize> = Vec::new();
+        comp_mapping.push(root);
+        let mut queue: Vec<usize> = vec![root];
+
+        while !queue.is_empty() {
+            let p = queue.pop().unwrap();
+            let mut edges: Vec<usize> = Vec::new();
+            for q in self.rev_default_edges[p].iter() {
+                match self.comp_idx[*q] {
+                    Some(_) => continue,
+                    None => {
+                        queue.push(*q);
+                        if let Some(x) = self.mapping[*q] {
+                            edges.push(x);
+                        }
+                        else {
+                            self.mapping[*q] = Some(comp_mapping.len());     //same here
+                            edges.push(comp_mapping.len());
+                            comp_mapping.push(*q);
+                            self.comp_idx[*q] = Some(self.components.len());
+                            edge_list.push(Vec::new());
+                        }
+                    }
+                };
+                
+            }
+            if p == root {
+                edge_list.push(edges);
+            }
+            else if let Some(x) = self.mapping[p] {
+                edge_list[x] = edges;
+            }
+        }
+        self.components.push(DefaultComp::new(CompType::Connected, edge_list, comp_mapping));
     }
 }
 
