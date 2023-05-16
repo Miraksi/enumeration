b142858e9aff0fbb35cf33833b921d7ab4a1db51
Arne Prinzler <arne.prinzler@stud.uni-goettingen.de>
2023-05-16 14:03:19 +0200

    added more complex test for default graphs

--- test_implementation/src/main.rs
+++ test_implementation/src/main.rs
@@ -5,11 +5,15 @@ use std::collections::HashMap;
 
 fn main() {
     let mut delta: Vec<HashMap<char,usize>> = Vec::new();
-    delta.push(HashMap::from([('a', 1), ('b', 2)]));
-    delta.push(HashMap::from([('a', 1), ('b', 2)]));
-    delta.push(HashMap::from([('b', 3)]));
-    delta.push(HashMap::from([('a', 1),('b', 4)]));
+    delta.push(HashMap::from([('a', 1), ('b', 3)]));
+    delta.push(HashMap::from([('a', 2)]));
     delta.push(HashMap::from([('b', 5)]));
+    delta.push(HashMap::from([('a', 4),('b', 3)]));
+    delta.push(HashMap::from([('a', 5),('b', 7)]));
+    delta.push(HashMap::from([('a', 6)]));
+    delta.push(HashMap::from([('a', 2)]));
+    delta.push(HashMap::from([('a', 5),('b', 8)]));
+    delta.push(HashMap::from([('b', 9)]));
     delta.push(HashMap::new());
     let default = DefaultGraph::new(&delta);
 
