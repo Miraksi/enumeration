88e12b5257e9540091e1d4b865c4c627d5a15c9d
Arne Prinzler <arne.prinzler@stud.uni-goettingen.de>
2023-06-13 18:49:12 +0200

    changed lca to not take ownership of the tree

--- test_implementation/src/beq/lca.rs
+++ test_implementation/src/beq/lca.rs
@@ -3,9 +3,6 @@ mod range_min;
 use range_min::RMQ;
 
 struct LCA {
-    root: usize,
-    parent: Vec<usize>,
-    children: Vec<Vec<usize>>,
     euler_tour: Vec<u32>,
     idx_map: Vec<usize>,    // stores the index of the Node of the euler tour to the original node
     last_occ: Vec<usize>,   // stores the last occurrence of out inital node in the tour
@@ -13,32 +10,45 @@ struct LCA {
 }
 
 impl LCA {
-    pub fn new(parent: Vec<usize>, children: Vec<Vec<usize>>, root: usize) -> Self {
-        let n = parent.len();
-        let mut tmp = Self {
-            root: root,
-            parent: parent,
-            children: children,
-            euler_tour: Vec::new(),
-            idx_map: Vec::new(),
-            last_occ: vec![0; n],
-            rmq: RMQ::new(vec![0,1,2,3]),
-        };
-        tmp.euler_dfs(root, 0);
-        tmp.rmq = RMQ::new(tmp.euler_tour.clone());
-        return tmp;
-    }
-    fn euler_dfs(&mut self, root: usize, height: u32) {
-        self.last_occ[root] = self.euler_tour.len();
-        self.euler_tour.push(height);
-        self.idx_map.push(root);
-        
-        for child in self.children[root].clone().iter() {
-            self.euler_dfs(*child, height + 1);
-            self.last_occ[root] = self.euler_tour.len();
-            self.euler_tour.push(height);
-            self.idx_map.push(root);
+    pub fn new(parent: &Vec<usize>, children: &Vec<Vec<usize>>, root: usize) -> Self {
+        let mut tour: Vec<u32> = Vec::new();
+        let mut map: Vec<usize> = Vec::new();
+        let mut last_occ: Vec<usize> = vec![0; parent.len()];
+        euler_dfs(&mut tour, &mut map, &mut last_occ, &parent, &children, root, 0);
+
+        Self {
+            euler_tour: tour.clone(),
+            idx_map: map,
+            last_occ: last_occ,
+            rmq: RMQ::new(tour),
         }
+    }
+
+    pub fn get(&self, i: usize, j: usize) -> usize {
+        let a = self.last_occ[i];
+        let b = self.last_occ[j];
+        return self.idx_map[self.rmq.get(a,b)];
+    }
+}
+
+fn euler_dfs(
+    tour: &mut Vec<u32>, 
+    map: &mut Vec<usize>, 
+    last_occ: &mut Vec<usize>, 
+    parent: &Vec<usize>, 
+    children: &Vec<Vec<usize>>, 
+    root: usize, 
+    height: u32) 
+    {
+    last_occ[root] = tour.len();
+    tour.push(height);
+    map.push(root);
+    
+    for child in children[root].clone().iter() {
+        euler_dfs(tour, map, last_occ, parent, children, *child, height + 1);
+        last_occ[root] = tour.len();
+        tour.push(height);
+        map.push(root);
     }
 }
 
