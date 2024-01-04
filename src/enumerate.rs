16ec34ecafbcb398a12b4c81e93a2db44cdb590f
arne.prinzler <arne.prinzler@stud.uni-goettingen.de>
2024-01-04 13:57:27 +0100

    removed tail-recursive approach

--- src/enumerate.rs
+++ src/enumerate.rs
@@ -32,25 +32,23 @@ impl Enumerate {
     }
 
     pub fn recurse(&self, a: char, q: usize, l: usize, indent: usize, stack_s: &mut Vec<(char,usize,usize)>, count: &mut usize, /*timing: &mut Instant*/) {
-        // println!("called with a: {a}, q: {q}, l: {l}");
         stack_s.push((a,q,l)); //2
-        //push stackframe of this call and top element of stack_s onto stack_c //3
-        let ind: String = "\t".repeat(indent);
+        // push stackframe of this call and top element of stack_s onto stack_c //3
         //println!("elapsed time: {:?}", timing.elapsed());
         //*timing = Instant::now();
+        // let ind: String = "\t".repeat(indent);
         // println!("{}Output: ({},{},{},{})",ind,self.n as i64 - l as i64 - 1,a,q,l);   //4
         *count += 1;
         if l == 0 {
             return; //5
         }
-        let mut last: (char, usize, usize) = ('a',0,0);
-        let mut u: VecDeque<((usize,usize),usize,usize)> = VecDeque::new(); //6
+        // let mut last: (char, usize, usize) = ('a',0,0);
+        let mut u: VecDeque<((usize,usize),usize)> = VecDeque::new(); //6
         if let Some((p,d)) = self.pmn.get(q,l) { //7
-            // println!("PathMaxNode({q},{l}) = ({p},{d})");
             let (b, w, new_p) = self.pmn.d_graph.lq[p][1];  //10
             if Val(d as i64) + w >= Val(l as i64) {     //8
-                u.push_back(((q, l), q, 0)); //9
-                last = (b, new_p, l-d-1);   //11
+                u.push_back(((q, l), 0)); //9
+                // last = (b, new_p, l-d-1);   //11
             }
             else {  //12
                 self.remove_this_call(stack_s);    //13
@@ -62,32 +60,24 @@ impl Enumerate {
             return;//14
         }
         while !u.is_empty() {   //16
-            let ((s,j),q,h) = u.pop_front().unwrap();   //17
-            // println!("popped (({s}, {j}), {q}, {h})");
+            let ((s,j),h) = u.pop_front().unwrap();   //17
             let (next_q, f) = self.pmn.get(s,j).unwrap(); //17
             if let Some((r,e)) = self.pmn.get(s,f) {    // 18
                 let w_r = self.pmn.d_graph.get_weight(r);
-                // println!("h+e+1+w_r = {:?}",Val(h as i64) + Val(e as i64) + Val(1) + w_r);
                 if f > 0 && Val(h as i64) + Val(e as i64) + Val(1) + w_r >= Val(l as i64) {
-                    // println!("pushed (({s}, {f}), {q}, {h})+");
-                    u.push_back(((s,f),q,h));
+                    u.push_back(((s,f),h));
                 }
             }
             let succ = self.pmn.d_graph.get_succesor(next_q).unwrap();
             if let Some((r,e)) = self.pmn.get(succ, j - f - 1) {   //19 fixed
                 let w_r = self.pmn.d_graph.get_weight(r);
                 if j - f - 1 > 0 && Val(h as i64)+Val(f as i64)+Val(1)+Val(e as i64)+Val(1)+w_r >= Val(l as i64) {
-                    // println!("pushed (({succ}, {}), {q}, {})-",j - f - 1, h + f + 1);
-                    u.push_back(((succ, j - f - 1), q, h + f + 1));
+                    u.push_back(((succ, j - f - 1), h + f + 1));
                 }
             }
-            let mut x = 2;  //22, 23        //third element
-            if ((s,j),q,h) != ((q,l),q,0) { //20
-                x = 1;  //21                //second element
-            }
+            let mut x = 1;
             let lq_ref = &self.pmn.d_graph.lq[next_q];
             for (b, g, branch) in lq_ref[x..].iter() {
-                // println!("h+f+g+1 = {:?}", Val(h as i64) + Val(f as i64) + *g + Val(1));
                 if Val(h as i64) + Val(f as i64) + *g + Val(1) < Val(l as i64) {
                     break;
                 }
@@ -96,7 +86,6 @@ impl Enumerate {
             }
         }
         stack_s.pop();
-        self.recurse(last.0, last.1, last.2, indent + 1, stack_s, count, /*timing*/);
         return;
     }
 
