dbd92a4b05ac54eb28177563fa7010f03d955b9e
Miraksi <arne.prinzler@gmail.com>
2024-01-09 14:20:54 +0100

    added benchmarking initialization (cycles)

--- benches/bench_init.rs
+++ benches/bench_init.rs
@@ -3,7 +3,36 @@ use test_implementation::enumerate::Enumerate;
 use criterion::*;
 use std::time::Duration;
 
-fn benchmark_init(c: &mut Criterion) {
+fn benchmark_circ(c: &mut Criterion) {
+    let mut delta: Vec<Vec<(char, usize)>> = Vec::new();
+
+    delta.push(vec![('a', 1)]);
+    delta.push(vec![('a', 2)]);
+    delta.push(vec![('a', 3)]);
+    delta.push(vec![('a', 4)]);
+    delta.push(vec![('a', 5)]);
+    delta.push(vec![('a', 6)]);
+    delta.push(vec![('a', 7)]);
+    delta.push(vec![('a', 8)]);
+    delta.push(vec![('a', 0)]);
+
+    let mut group = c.benchmark_group("benchmark_circ");
+    for x in (10..=60) {
+        let len = delta.len();
+        delta[len-1][0].1 = len;
+        for i in 0..1 {
+            delta.push(vec![('a', len + i + 1)]);
+        }
+        delta[len][0].1 = 0;
+        group.bench_with_input(BenchmarkId::from_parameter(delta.len()), &delta, |b, delta| b.iter(|| {
+            Enumerate::new(delta.clone());
+        }));
+        println!("n = {}", delta.len());
+    }
+}
+
+
+fn benchmark_rec(c: &mut Criterion) {
     let mut delta: Vec<Vec<(char, usize)>> = Vec::new();
 
     delta.push(vec![('a', 1), ('b', 4)]);
@@ -32,7 +61,7 @@ fn benchmark_init(c: &mut Criterion) {
     delta.push(vec![]);
     let enumerate = Enumerate::new(delta.clone());
 
-    let mut group = c.benchmark_group("benchmark_init");
+    let mut group = c.benchmark_group("benchmark_rec");
     for x in (10..=45).step_by(5) {
         let n: usize = x;
         let mut count = 0;
@@ -46,7 +75,7 @@ fn benchmark_init(c: &mut Criterion) {
 criterion_group!{
     name = benches;
     // This can be any expression that returns a `Criterion` object.
-    config = Criterion::default().measurement_time(Duration::new(30,0));
-    targets = benchmark_init
+    config = Criterion::default().measurement_time(Duration::new(4,0));
+    targets = benchmark_circ
 }
 criterion_main!(benches);
